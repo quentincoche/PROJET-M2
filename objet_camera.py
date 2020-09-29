4e65a7b771279e67b8e5d10c75830b3a6919f8e8
cor <marin.cor@etudiant.univ-rennes1.fr>
2020-09-29 10:07:41 +0200

    objet fonctionnel

--- objet_camera.py
+++ objet_camera.py
@@ -5,7 +5,6 @@ Tested on Basler acA1300-200uc (USB3, linux 64bit , python 3.5)
 '''
 from pypylon import pylon
 import cv2
-import numpy as np
 
 class Camera():
     
@@ -13,15 +12,15 @@ class Camera():
         # conecting to the first available camera
         self.camera = pylon.InstantCamera(pylon.TlFactory.GetInstance().CreateFirstDevice())
         self.camera.Open()
-        self.temp_exp=16.0
-        self.acquisition()
+        #self.temp_exp=16.0
+        #self.acquisition()
     
     def acquisition(self):
         self.camera.AcquisitionMode.SetValue('Continuous') #SingleFrame
         self.camera.PixelFormat.SetValue('Mono12')
         self.camera.GainAuto.SetValue("Continuous")
         self.camera.AcquisitionFrameRate.SetValue(60.0)
-        self.camera.ExposureAuto.SetValue('Off')#Continuous, SingleFrame
+        self.camera.ExposureAuto.SetValue('Continuous')#Continuous, SingleFrame
         self.camera.ExposureTime.SetValue(self.temp_exp)
         # Grabing Continusely (video) with minimal delay
         self.camera.StartGrabbing(pylon.GrabStrategy_LatestImageOnly) 
@@ -34,8 +33,8 @@ class Camera():
                 # Access the image data
                 self.image = self.converter.Convert(self.grabResult)
                 self.img = self.image.GetArray()
-                self.auto_exposure()
                 #Demande d'auto-exposition
+                self.camera.ExposureTime.SetValue(self.temp_exp)
                 cv2.namedWindow('title', cv2.WINDOW_NORMAL)
                 cv2.imshow('title', self.img)
                 self.k = cv2.waitKey(1)
@@ -60,39 +59,3 @@ class Camera():
         self.camera.StopGrabbing()
         self.camera.Close()
         cv2.destroyAllWindows()
-        
-    def auto_exposure(self):
-        exp_time_ok=False
-        max_img=self.maximum(self.img)
-        while exp_time_ok==False:
-            if max_img <=3500:
-                tps=round(self.camera.ExposureTime.Min*2.)
-            elif max_img >=4095:
-                tps=round(self.camera.ExposureTime.Min/1.5)
-        else :
-            exp_time_ok=True
-            self.temp_exp=tps
-            
-        if self.temp_exp <=16:
-            print("Temps d'exposition trop court")
-        elif self.temp_exp>=10000000:
-            print("Temps d'exposition trop haut")
-        return(self.temp_exp)
-    
-    def maximum(self,liste):
-        m=[]
-        l=len(liste)
-        for i in range (l):
-            L=len(liste[i])
-            for j in range (L):
-                m.append(liste[i][j])
-        print(max(m))
-        return(max(m))
-    
-    def function(self):
-        if self.camera.ExposureAuto.GetValue() == 'Off':
-                x=input("Auto exposition ? (y ou n) :")
-                if x=='y':
-                    self.auto_exposure()
-                else :
-                    self.temp_exp=int(input("Temps d'exposition (µs) :"))
