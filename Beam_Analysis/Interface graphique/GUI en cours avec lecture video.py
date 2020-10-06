2206270e77304bcca644d5b5aebc60199efefc83
Marin-c <71694829+Marin-c@users.noreply.github.com>
2020-10-06 09:45:38 +0200

    début de commentaire et adaptation de la fonction de nettoyage

--- Beam_Analysis/Interface graphique/GUI en cours avec lecture video.py
+++ Beam_Analysis/Interface graphique/GUI en cours avec lecture video.py
@@ -4,41 +4,49 @@ Created on Mon Sep 28 09:22:21 2020
 
 @author: Optique
 """
-from pypylon import pylon
-from PIL import Image as Img
+from pypylon import pylon #Bibliothèque Basler d'interfaçage de la caméra
+from PIL import Image as Img #Bibliothèque de traitement d'image
 from PIL import ImageTk
-from tkinter import *
-import cv2
+from tkinter import * #Bibliothèque de GUI
+import cv2 #Bibliothèque d'interfaçage de caméra et de traitement d'image
 import tkinter as tk
-import time
-import datetime
-import os
-import numpy as np
-import matplotlib.pyplot as plt
+import time #Bibliothèque permettant d'utiliser l'heure de l'ordinateur
+import datetime #Bibliothèque permettant de récupérer la date
+import os #Bibliothèque permettant de communiquer avec l'os et notamment le "path"
+import numpy as np #Bibliothèque de traitement des vecteurs et matrice
+import matplotlib.pyplot as plt #Bibliothèque d'affichage mathématiques
 
+#####################################################################
+#                                                                   #
+#           Programme d'interfaçage de faisceaux                    #
+#                                                                   #
+#####################################################################
+
+
+#La Classe Fenetre contient l'ensemble du programme
 
 class Fenetre():
     cam0 = int(input("Port de périphérique USB de la caméra : "))
     propre=input('Veux-tu avoir une image propre (True/False) : ')
-    def __init__(self, output_path = "./"):
+    def __init__(self, output_path = "./"): #Fonction d'initialisation du programme
 
         self.output_path = output_path  # chemin de sortie de la photo
 
         """Initialisation de la camera"""
         self.cap0 = cv2.VideoCapture(self.cam0) # Acquisition du flux vidéo des périphériques
-        self.temp_exp=50.0
-        self.auto_exposure()
+        self.temp_exp=50.0 #Définition d'un temps d'exposition volontairement faible qui sera ajuster ensuite
+        self.auto_exposure() #Lance le programme d'auto-exposition
         self.cap0.set(3, 5472) # Redéfinition de la taille du flux
-        self.cap0.set(4, 3648) # Max (1920 par 1080)
-        self.cap0.set(cv2.CAP_PROP_AUTO_EXPOSURE,0.75)
+        self.cap0.set(4, 3648) # Max (5472 par 3648)
+        self.cap0.set(cv2.CAP_PROP_AUTO_EXPOSURE,0.75) #On utilise pas l'auto-exposition d'opencv
         
         """"Edition de l'interface"""
         self.window = tk.Tk()  #Réalisation de la fenêtre principale
-        self.window.geometry("920x613")
+        self.window.geometry("920x613") #taille de la fenetre
          
         self.window.title("Beam analyzer Python")
         self.window.config(background="#FFFFFF") # Couleur de la fenêtre
-        self.window.protocol('WM_DELETE_WINDOW', self.destructor)
+        self.window.protocol('WM_DELETE_WINDOW', self.destructor) #La croix de la fenetre va fermer le programme
 
         """"definition des proportions pour les frames"""
         #self.window.grid_columnconfigure(0, weight=1)
@@ -48,10 +56,12 @@ class Fenetre():
 
 
         self.Interface() #Lance la fonction Interface
-        self.video_loop()
+        self.video_loop() #lance la fonction d'acquisition de la caméra
     
 ##########################################    
     def Interface(self):
+        #Fonction permettant de créer l'interface dans laquelle sera placé toutes les commandes et visualisation permettant d'utiliser le programme
+        
         #commandes gauche
         self.cmdleft = tk.Frame(self.window,padx=5,pady=5,bg="red")
         self.cmdleft.grid(row=1,column=0, sticky='NSEW')
@@ -88,15 +98,20 @@ class Fenetre():
         self.window.destroy() # Ferme la fenêtre
 
     def auto_exposure(self):
+        #Fonction d''auto-exposition uniquement pour la caméra Basler actuellement
+
         self.cap0.release()  # lâche le flux vidéo
-        self.camera = pylon.InstantCamera(pylon.TlFactory.GetInstance().CreateFirstDevice())
-        self.camera.Open()
-        self.camera.ExposureTime.SetValue(self.temp_exp)
+        self.camera = pylon.InstantCamera(pylon.TlFactory.GetInstance().CreateFirstDevice())    #récupère la caméra par le biais de la bibliothèque Pylon
+        self.camera.Open() #Ouvre la communication avec la caméra
+        self.camera.ExposureTime.SetValue(self.temp_exp) #établi le temps d'exposition
         self.camera.ExposureAuto.SetValue('Off')#Continuous, SingleFrame
-        self.camera.AcquisitionMode.SetValue('SingleFrame')
-        exp_ok=False
-        max=self.max_photo()
-        while exp_ok == False:
+        self.camera.AcquisitionMode.SetValue('SingleFrame') #Utilise la caméra en mode photo
+        
+        exp_ok=False #Variable permettant de définir l'état de l'ajustement de l'exposition
+        
+        max=self.max_photo() #variable du max d'intensité de l'image
+
+        while exp_ok == False: #Définit l'augmentation ou la diminution des valeurs d'exposition en fonction du max d'intensité de l'image
             if max<=150:
                 self.temp_exp=self.temp_exp*2.
                 max=self.max_photo()
@@ -116,8 +131,8 @@ class Fenetre():
             else:
                 exp_ok=True
                 break
-        self.camera.Close()
-        self.cap0 = cv2.VideoCapture(self.cam0)
+        self.camera.Close() #Ferme la communication avec la caméra
+        self.cap0 = cv2.VideoCapture(self.cam0) #Lance l'acquisition avec le module opencv
         return
                 
     def max_photo(self):
@@ -168,7 +183,7 @@ class Fenetre():
         if self.propre=="False" :
             raise Exception()
         else :
-            #self.frame=cv2.cvtColor(self.frame0, cv2.COLOR_BGR2GRAY)
+            self.frame=cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
             self.blur = cv2.GaussianBlur(self.frame,(5,5),0)
             ret3,self.frame = cv2.threshold(self.blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
         return
