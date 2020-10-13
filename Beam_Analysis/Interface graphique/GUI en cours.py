4e31743d2517f1ffea96589abaae92214c088f3d
quentincoche <71694799+quentincoche@users.noreply.github.com>
2020-10-13 15:11:09 +0200

    Update GUI en cours.py

--- Beam_Analysis/Interface graphique/GUI en cours.py
+++ Beam_Analysis/Interface graphique/GUI en cours.py
@@ -35,7 +35,7 @@ class Fenetre():
         """"Edition de l'interface"""
         self.window = tk.Tk()  #Réalisation de la fenêtre principale
         self.window.geometry("920x613") #taille de la fenetre
-         
+
         self.window.title("Beam analyzer Python")
         self.window.config(background="#FFFFFF") # Couleur de la fenêtre
         self.window.protocol('WM_DELETE_WINDOW', self.destructor) #La croix de la fenetre va fermer le programme
@@ -53,11 +53,11 @@ class Fenetre():
         self.delay=15
         #self.vid.__init__()
         #self.update() #boucle la fonction d'acquisition de la caméra
-    
-##########################################    
+
+##########################################
     def Interface(self):
         """ Fonction permettant de créer l'interface dans laquelle sera placé toutes les commandes et visualisation permettant d'utiliser le programme """
-        
+
         #commandes gauche
         self.cmdleft = tk.Frame(self.window,padx=5,pady=5,bg="red")
         self.cmdleft.grid(row=1,column=0, sticky='NSEW')
@@ -67,7 +67,7 @@ class Fenetre():
         btncap.grid(row=0,column=0,sticky="nsew")
         btnquit = tk.Button(self.cmdleft,text="Quitter",command = self.destructor)
         btnquit.grid(row=1,column=0,sticky="nsew")
-        
+
         #commandes superieures
         self.cmdup = tk.Frame(self.window,padx=5,pady=5,bg="blue")
         self.cmdup.grid(row=0,column=1, sticky="NSEW")
@@ -79,51 +79,51 @@ class Fenetre():
         btnexp.grid(row=0,column=1,sticky="nsew")
 
         #cadre video
-        
+
         self.display1 = tk.Canvas(self.window, width=self.Screen_x,height=self.Screen_y, bg="green")  # Initialisation de l'écran 1
-        self.display1.bind("<Configure>",self.auto_size)
+        self.display1.bind("<Configure>",self.update)
         self.display1.grid(row=1,column=1,sticky="NSEW")
         self.display1.grid_columnconfigure(0,weight=1)
         self.display1.grid_rowconfigure(0,weight=1)
 
-        
-    
-##########################################    
-    
+
+
+##########################################
+
     def destructor(self):
         """ Détruit les racines objet et arrête l'acquisition de toutes les sources """
         print("[INFO] closing...")
         self.window.destroy() # Ferme la fenêtre
 
-    def auto_size(self,event):
-        """redimensionnement de l'image pour correspondre à la taille de la fenêtre"""
+    # def auto_size(self,event):
+    #     """redimensionnement de l'image pour correspondre à la taille de la fenêtre"""
+    #     frame = self.vid.getFrame() #This is an array
+    #     try:
+    #         self.display1.delete(self.display1.image)
+    #     except:
+    #         pass
+    #     self.photo=cv2.resize(frame,dsize=(event.width,event.height), interpolation=cv2.INTER_CUBIC)
+    #     #OpenCV bindings for Python store an image in a NumPy array
+    #     #Tkinter stores and displays images using the PhotoImage class
+    #     # Use PIL (Pillow) to convert the NumPy ndarray to a PhotoImage
+    #     self.photo = ImageTk.PhotoImage(image = Img.fromarray(frame))
+    #     self.display1.create_image(event.width/2,event.height/2,image=self.photo)
+    #     self.display1.bind("<Configure>",self.auto_size)
+
+    #     self.window.after(self.delay, self.update)
+    def update(self,event):
+        #Get a frame from cameraCapture
         frame = self.vid.getFrame() #This is an array
-        try:
-            self.display1.delete(self.display1.image)
-        except:
-            pass
-        self.photo=cv2.resize(frame,dsize=(event.width,event.height), interpolation=cv2.INTER_CUBIC) 
+        #https://stackoverflow.com/questions/48121916/numpy-resize-rescale-image/48121996
+        frame = cv2.resize(frame, dsize=(event.width,event.height), interpolation=cv2.INTER_CUBIC)
+
         #OpenCV bindings for Python store an image in a NumPy array
         #Tkinter stores and displays images using the PhotoImage class
         # Use PIL (Pillow) to convert the NumPy ndarray to a PhotoImage
         self.photo = ImageTk.PhotoImage(image = Img.fromarray(frame))
         self.display1.create_image(event.width/2,event.height/2,image=self.photo)
-        self.display1.bind("<Configure>",self.auto_size)
 
-        self.window.after(self.delay, self.auto_size)
-    # def update(self):
-    #     #Get a frame from cameraCapture
-    #     frame = self.vid.getFrame() #This is an array
-    #     #https://stackoverflow.com/questions/48121916/numpy-resize-rescale-image/48121996
-    #     frame = cv2.resize(frame, dsize=(Screen_x, Screen_Y), interpolation=cv2.INTER_CUBIC)
-
-    #     #OpenCV bindings for Python store an image in a NumPy array
-    #     #Tkinter stores and displays images using the PhotoImage class
-    #     # Use PIL (Pillow) to convert the NumPy ndarray to a PhotoImage
-    #     self.photo = ImageTk.PhotoImage(image = Img.fromarray(frame))
-    #     self.display1.create_image(500,300,image=self.photo)
-
-    #     self.window.after(self.delay, self.update)
+        self.window.after(self.delay, self.update)
 
     def capture(self):
         """ Fonction permettant de capturer une image et de l'enrigistré avec l'horodatage """
@@ -157,7 +157,7 @@ class Fenetre():
 
             #Remet l'image en RGB pour y dessiner toutes les formes par la suite et en couleur
             self.frame = cv2.cvtColor(self.frame, cv2.COLOR_GRAY2RGB)
-            
+
             # find contours in the binary image
             contours, hierarchy = cv2.findContours(self.otsu,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
             for c in contours:
@@ -179,7 +179,7 @@ class Fenetre():
                     break
                 ellipse = cv2.fitEllipse(c)
                 thresh = cv2.ellipse(self.frame,ellipse,(0,255,0),1)
-                
+
             M=cv2.moments(self.otsu)
             # calculate x,y coordinate of center
             cX = int(M["m10"] / M["m00"])
@@ -187,12 +187,12 @@ class Fenetre():
 
             # dessine les contours des formes qu'il a identifiés
             cv2.drawContours (self.frame, contours, 3, (255,215,0), 3)
-            
+
             #Dessine une croix sur le barycentre de l'image
             cv2.line(self.frame, (cX, 0), (cX, height), (255, 0, 0), 1)
             cv2.line(self.frame, (0, cY), (width, cY), (255, 0, 0), 1)
 
-        return        
+        return
 
 
 root = Fenetre()
