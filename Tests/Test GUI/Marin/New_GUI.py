83744fc689e0b97fb7f839c7da6a73006e0fc2d3
Marin-c <71694829+Marin-c@users.noreply.github.com>
2020-11-30 11:54:43 +0100

    /

--- Tests/Test GUI/Marin/New_GUI.py
+++ Tests/Test GUI/Marin/New_GUI.py
@@ -32,7 +32,7 @@ Created on Mon Sep 28 09:22:21 2020
 #   * Affichage des courbes
 
 
-
+print("[INFO] starting...")
 from pypylon import pylon #Bibliothèque Basler d'interfaçage de la caméra
 from PIL import Image as Img #Bibliothèque de traitement d'image
 from PIL import ImageTk
